d98321295c5bb136c73da8d1fe2d76cb4c0e96c1
arshqiii <marshq23@gmail.com>
2024-11-08 07:04:00 +0700

    moved ItemHomepage & ItemCard to product_card.dart

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sinibeli_mobile/widgets/left_drawer.dart';
+import 'package:sinibeli_mobile/widgets/product_card.dart';
 
 class MyHomePage extends StatelessWidget {
   final String npm = '2306275885';
@@ -151,100 +152,6 @@ class InfoCard extends StatelessWidget {
               ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class ItemHomepage {
-  final String name;
-  final IconData icon;
-  final Color color;
-
-  ItemHomepage(this.name, this.icon, this.color); //constructor
-}
-
-class ItemCard extends StatefulWidget {
-  //* Menampilkan kartu dengan ikon dan nama.
-  final ItemHomepage item;
-
-  const ItemCard(this.item, {super.key}); //constructor
-
-  @override
-  State<ItemCard> createState() => _ItemCardState();
-}
-
-class _ItemCardState extends State<ItemCard> {
-  bool isPressed = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTapDown: (_) {
-        setState(() {
-          isPressed = true;
-        });
-      },
-      onTapUp: (_) {
-        setState(() {
-          isPressed = false;
-        });
-        // menampilkan snackbar setelah animasi selesai
-        ScaffoldMessenger.of(context)
-          ..hideCurrentSnackBar()
-          ..showSnackBar(
-            SnackBar(
-              content: Text(
-                "Kamu telah menekan tombol ${widget.item.name}!",
-                style: const TextStyle(fontSize: 14),
-              ),
-              behavior: SnackBarBehavior.floating,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              margin: const EdgeInsets.all(12),
-            ),
-          );
-      },
-      onTapCancel: () {
-        setState(() {
-          isPressed = false;
-        });
-      },
-      child: AnimatedSlide( //animasi ketika itemCard ditekan
-        duration: const Duration(milliseconds: 100),
-        offset: Offset(0, isPressed ? 0.05 : 0),
-        child: Material(
-          color: widget.item.color,
-          borderRadius: BorderRadius.circular(16),
-          elevation: isPressed ? 1 : 2,
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(
-                  widget.item.icon,
-                  color: Colors.white,
-                  size: 32.0,
-                ),
-                const SizedBox(height: 8),
-                Text(
-                  widget.item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 12,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis, //handle kasus overflow
-                ),
-              ],
-            ),
-          ),
         ),
       ),
     );
